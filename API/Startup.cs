596326d7969eb6d7e7740371fd14424bfddd1f0a
Aruru <sudheer.aruru@monster.com>
2022-05-15 12:31:09 +0800

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,7 +23,12 @@ namespace API
             services.AddDbContext<StoreContext>(x => 
                     x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();   
-            services.AddSwaggerDocumentation();         
+            services.AddSwaggerDocumentation();
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });      
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +43,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -27,14 +27,19 @@ namespace Infrastructure.Data
             return await ApplySpecification(spec).ToListAsync();
         }
 
-        async Task<T> IGenericRepository<T>.GetByIdAsync(int id)
+        public async Task<T> GetByIdAsync(int id)
         {
             return await _context.Set<T>().FindAsync(id);
         }
 
-        async Task<IReadOnlyList<T>> IGenericRepository<T>.ListAllAsync()
+        public async Task<IReadOnlyList<T>> ListAllAsync()
         {
             return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
         }
 
         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
